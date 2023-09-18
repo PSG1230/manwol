c9cc440a44d49622f5ad35713484f33e25c14919
PSG1230 <92858913+PSG1230@users.noreply.github.com>
2023-09-18 15:36:01 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+					$("#button1").click(function(){
+
+							$(".img1").fadeIn().siblings().fadeOut();
+					});	
+					$("#button2").click(function(){
+
+							$(".img2").fadeIn().siblings().fadeOut();
+					});	
+					$("#button3").click(function(){
+
+							$(".img3").fadeIn().siblings().fadeOut();
+					});	
+					$("#button4").click(function(){
+
+							$(".img4").fadeIn().siblings().fadeOut();
+					});	
+			});
